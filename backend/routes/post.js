12038d657d5dddbb08a8b19b3ac5f2df789819f2
Sourav <sourav862888@gmail.com>
2024-03-11 10:17:49 +0530

    follow unfollow api done

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const { createPost, likeAndUnlikePost } = require("../controllers/post");
+const {
+  createPost,
+  likeAndUnlikePost,
+  deletePost,
+} = require("../controllers/post");
 const { isAuthenticated } = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.route("/post/upload").post(isAuthenticated, createPost);
-router.route("/post/:id").post(isAuthenticated, likeAndUnlikePost);
+router
+  .route("/post/:id")
+  .get(isAuthenticated, likeAndUnlikePost)
+  .delete(isAuthenticated, deletePost);
+// router.route("/post/:id").delete(isAuthenticated, deletePost);
 
 module.exports = router;
